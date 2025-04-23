1de2361d44e9e435748d1b4590892d377c9362f5
Nguyen Thanh Dat <khangvskiss0@gmail.com>
2025-04-24 02:26:18 +0700

    version2.0

--- BaiTapLon/app/src/main/java/com/example/tramxeuth/MainActivity.kt
+++ BaiTapLon/app/src/main/java/com/example/tramxeuth/MainActivity.kt
@@ -1,42 +1,15 @@
     package com.example.tramxeuth
 
-import android.content.ContentValues.TAG
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.tramxeuth.Data.UserRepository
 import com.example.tramxeuth.Route.routeScreen
-import com.example.tramxeuth.View.SignForm
 import com.example.tramxeuth.ViewModel.AuthViewModel
 import com.example.tramxeuth.ViewModel.FirebaseViewModel
 import com.example.tramxeuth.ViewModel.UserViewModel
-import com.example.tramxeuth.ui.theme.TramXeUTHTheme
-import com.google.firebase.Firebase
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.database
-import com.google.firebase.database.getValue
-import com.google.firebase.firestore.FirebaseFirestore
-import java.time.LocalDate
-import java.time.LocalDateTime
 
     class MainActivity : ComponentActivity() {
         @RequiresApi(Build.VERSION_CODES.O)
@@ -49,6 +22,7 @@ import java.time.LocalDateTime
             enableEdgeToEdge()
             setContent {
                 routeScreen(authViewModel, userViewModel, firebaseViewModel)
+//                MainScreen()
             }
         }
     }
